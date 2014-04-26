8ac6eb91291cc381e3e1bb54ae7e0e8267576b54
toonya <msc5762@me.com>
2014-04-26 23:00:55 +0800

    changeable quote

--- admin/js/fn.js
+++ admin/js/fn.js
@@ -1,10 +1,10 @@
 (function($){
 
 	"use strict";
-
+	// 新描述
 	$('form').on('click', '.new_desc', function(e){
 		e.preventDefault();
-		var $new_item = $(this).closest('.changable-item').clone(),
+		var $new_item = $(this).closest('.changeable-item').clone(),
 			$input = $new_item.find('input'),
 			index = $input.data('index'),
 			name = $input.data('name');
@@ -14,10 +14,10 @@
 			'data-index': index+1
 			});
 		$new_item.find('.desc-index').text(index+1+1);
-		$(this).closest('.changable-item').after($new_item).find('#new_desc').remove();
+		$(this).closest('.changeable-item').after($new_item).find('.new_desc').remove();
 	})
-
-	$('form').on('change', '.quote_type', function(e){
+	// 切换报价类型
+	.on('change', '.quote_type', function(e){
 		var for_target = $(this).find(':selected').data('for');
 		if( for_target && $('[data-for="'+for_target+'"]') ) {
 			$(this).closest('.item').find('.quote_input').not('[data-for="'+for_target+'"]').addClass('hide');
@@ -27,6 +27,27 @@
 			$(this).closest('.item').find('.quote_input').not('[data-for]').removeClass('hide');
 			$(this).closest('.item').find('.quote_input[data-for]').addClass('hide');
 		}
+	})
+	// 新报价
+	.on('click', '.item .new_quote', function(e){
+		e.preventDefault();
+
+		var $new_item = $(this).closest('.item').clone(),
+			$input = $new_item.find('input');
+		$input.each(function(i,e){
+			var $input = $(this),
+			index = $input.data('index'),
+			name = $input.data('name');
+			$input.attr({
+				'name': name+"[" + (index+1) + "]" ,
+				'data-index': index+1
+				});
+		});
+
+		console.log();
+		$new_item.find('.quote-index').text( $new_item.find('.quote-index').text()/1+1 );
+
+		$(this).closest('.item').after($new_item).find('.new_quote').remove();
 	})
 
 })(jQuery)
