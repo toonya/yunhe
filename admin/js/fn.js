61116803d1537858a57172858fd664650cd96356
toonya <msc5762@me.com>
2014-04-26 22:09:24 +0800

    Quote input groups.

--- admin/js/fn.js
+++ admin/js/fn.js
@@ -2,7 +2,7 @@
 
 	"use strict";
 
-	$('form').on('click', '#new_desc', function(e){
+	$('form').on('click', '.new_desc', function(e){
 		e.preventDefault();
 		var $new_item = $(this).closest('.changable-item').clone(),
 			$input = $new_item.find('input'),
@@ -15,6 +15,18 @@
 			});
 		$new_item.find('.desc-index').text(index+1+1);
 		$(this).closest('.changable-item').after($new_item).find('#new_desc').remove();
+	})
+
+	$('form').on('change', '.quote_type', function(e){
+		var for_target = $(this).find(':selected').data('for');
+		if( for_target && $('[data-for="'+for_target+'"]') ) {
+			$(this).closest('.item').find('.quote_input').not('[data-for="'+for_target+'"]').addClass('hide');
+			$(this).closest('.item').find('.quote_input[data-for="'+for_target+'"]').removeClass('hide');
+		}
+		else {
+			$(this).closest('.item').find('.quote_input').not('[data-for]').removeClass('hide');
+			$(this).closest('.item').find('.quote_input[data-for]').addClass('hide');
+		}
 	})
 
 })(jQuery)
